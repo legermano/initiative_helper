32c9d1ce50325041451386341edf6783f51580b2
Lucas Emanuel Germano <lucas.germano@universo.univates.br>
2020-07-02 18:01:55 -0300

    Wrap  the character card with slidable

--- lib/app/modules/home/widgets/character_card.dart
+++ lib/app/modules/home/widgets/character_card.dart
@@ -1,8 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_slidable/flutter_slidable.dart';
 import 'package:initiative_helper/app/database/database.dart';
 import 'package:initiative_helper/app/modules/home/home_controller.dart';
 import 'package:initiative_helper/app/modules/home/widgets/character_dialog.dart';
 import 'package:initiative_helper/colors/custom_colors.dart';
+
+import '../../../../colors/custom_colors.dart';
 
 class CharacterCard extends StatelessWidget {
   final HomeController controller;
@@ -14,54 +17,70 @@ class CharacterCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     Character ch = character.character;
+
     return Card(
       color: character.turn ? Colors.amber[100] : Colors.white,
-      child: Padding(
-        padding: const EdgeInsets.all(8),
-        child: Row(
-          mainAxisSize: MainAxisSize.max,
-          children: [
-            Expanded(
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                mainAxisSize: MainAxisSize.min,
-                children: [
-                  Text(ch.name),
-                  Row(
+      child: Slidable(   
+        key: Key(ch.id.toString()),
+        actionPane: SlidableDrawerActionPane(),
+        actionExtentRatio: 0.25,
+        child: Padding(
+          padding: const EdgeInsets.all(8),
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: [
+              Expanded( 
+                child: Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    mainAxisSize: MainAxisSize.min,
                     children: [
-                      Text(
-                        'Initiative : '+ch.initiative.toString(),
-                        style: const TextStyle(fontSize: 12),
-                      ),
-                      SizedBox(width: 8,),
-                      Text(
-                        'Modifier : '+ch.modifier.toString(),
-                        style: const TextStyle(fontSize: 12),
+                      Text(ch.name),
+                      Row(
+                        children: [
+                          Text(
+                            'Initiative : '+ch.initiative.toString(),
+                            style: const TextStyle(fontSize: 12),
+                          ),
+                          SizedBox(width: 8,),
+                          Text(
+                            'Modifier : '+ch.modifier.toString(),
+                            style: const TextStyle(fontSize: 12),
+                          )
+                        ],
                       )
                     ],
-                  )
-                ],
+                  ),
+                )
               )
-            ),
-            IconButton(
-              icon: const Icon(Icons.edit), 
-              color: Colors.blue,
-              onPressed: () {
-                showDialog(
-                  context: context,
-                  barrierDismissible: false,
-                  builder: (context) => CharacterDialog(controller: controller,character: character),
-                );
-              }
-            ),
-            IconButton(
-              icon: const Icon(Icons.delete), 
-              color: CustomColor.red,
-              onPressed: () {
-                controller.deleteCharacter(character);
-              }
-            )
-          ],
+            ],
+          ),
+        ),
+        secondaryActions: [
+          IconSlideAction(
+            caption: 'Edit',
+            color: Colors.grey[400],
+            icon: Icons.more_horiz,
+            foregroundColor: Colors.white,
+            onTap: () {
+              showDialog(
+                context: context,
+                barrierDismissible: false,
+                builder: (context) => CharacterDialog(controller: controller,character: character),
+              );
+            },
+          ),
+          IconSlideAction(
+            caption: 'Delete',
+            color: CustomColor.red,
+            icon: Icons.delete,
+            onTap: () => controller.deleteCharacter(character),
+          )
+        ],
+        dismissal: SlidableDismissal(
+          child: SlidableDrawerDismissal(),
+          onDismissed: (actionType) => controller.deleteCharacter(character),
         ),
       ),
     );
